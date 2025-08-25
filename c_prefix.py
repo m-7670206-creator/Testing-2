dac268356ddadef9a698695a091624f4051bc43e
m-7670206-creator <m-7670206@moe-dl.edu.my>
2025-08-25 17:34:13 +0800

    Add files via upload

---
+++ c_prefix.py
@@ -0,0 +1,23 @@
+strs = ["abc", "abcde", "abcdefg"]
+prev = ""
+common = []
+
+for n, chr in enumerate(strs[0]):
+
+    if chr in strs[1] and chr in strs[2]:
+        prev = prev + chr
+
+    elif prev not in common and prev:
+        common.append(prev)
+
+        prev = ""
+
+    if n == len(strs[0]) - 1:
+        common.append(prev)
+
+longest = ""
+
+for term in common:
+    if len(term) > len(longest):
+        longest = term
+print(longest)
